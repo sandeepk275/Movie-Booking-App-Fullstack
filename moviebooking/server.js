a9f81f6b9da303284b865d8bd35d4bfd731e908f
sandeepk275 <sandeep.kumar41991@gmail.com>
2022-02-09 16:26:34 +0530

    checkpoint 3

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -3,9 +3,11 @@ const app=express();
 
 const PORT=9000;
 
+
+
+
 app.get('/movies',(req, res)=>{
     res.send("All Movies Data in JSON format from Mongo DB");
-
 })
 app.get('/genres', (req, res) => {
     res.send("All Genres Data in JSON format from Mongo DB");
@@ -14,6 +16,7 @@ app.get('/artists', (req, res) => {
     res.send("All Artists Data in JSON format from Mongo DB");
 
 })
+
 
 app.listen(PORT, () => {
     console.log(`server start listening to  port ${PORT}`);
